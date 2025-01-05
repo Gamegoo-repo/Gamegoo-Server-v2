1cb3ce44376fbe832f285991d253df8ee5365949
Rimi <lora3226@inu.ac.kr>
2025-01-05 18:33:47 +0900

    🚑 [HotFix] CORS 에러 해결

--- src/main/java/com/gamegoo/gamegoo_v2/core/config/WebConfig.java
+++ src/main/java/com/gamegoo/gamegoo_v2/core/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.gamegoo.gamegoo_v2.account.auth.jwt.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,6 +34,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowCredentials(true); // 쿠키 인증 요청 허용
     }
 
 }
