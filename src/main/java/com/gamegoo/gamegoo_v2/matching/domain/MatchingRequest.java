d055db254972130ca6b8079c78a1e39cfaa912e5
Rimi <lora3226@inu.ac.kr>
2025-01-21 19:43:55 +0900

    :sparkles: [Feat] MatchingResult 추가

--- src/main/java/com/gamegoo/gamegoo_v2/matching/domain/MatchingRequest.java
+++ src/main/java/com/gamegoo/gamegoo_v2/matching/domain/MatchingRequest.java
@@ -5,6 +5,7 @@ import com.gamegoo.gamegoo_v2.account.member.domain.Mike;
 import com.gamegoo.gamegoo_v2.account.member.domain.Position;
 import com.gamegoo.gamegoo_v2.account.member.domain.Tier;
 import com.gamegoo.gamegoo_v2.core.common.BaseDateTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,10 +16,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -67,18 +72,32 @@ public class MatchingRequest extends BaseDateTimeEntity {
     @Column
     private int freeRank;
 
-    @Column(nullable = false)
+    @Column
     private double freeWinRate;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private MatchingType matchingType;
+
+    @Column
+    private int mannerLevel;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
+    @OneToMany(mappedBy = "matching_request_sender", cascade = CascadeType.ALL)
+    private List<MatchingResult> matchingResultSenderList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "matching_request_receiver", cascade = CascadeType.ALL)
+    private List<MatchingResult> matchingResultReceiverList = new ArrayList<>();
+
+
     // MatchingRequest 생성 메서드
     public static MatchingRequest create(GameMode gameMode, Position mainPosition, Position subPosition,
                                          Position wantPosition, Mike mike, Tier soloTier, int soloRank,
                                          double soloWinRate, Tier freeTier, int freeRank, double freeWinRate,
-                                         Member member) {
+                                         MatchingType matchingType, int mannerLevel, Member member) {
         return MatchingRequest.builder()
                 .gameMode(gameMode)
                 .mainPosition(mainPosition)
@@ -91,6 +110,8 @@ public class MatchingRequest extends BaseDateTimeEntity {
                 .freeTier(freeTier)
                 .freeRank(freeRank)
                 .freeWinRate(freeWinRate)
+                .matchingType(matchingType)
+                .mannerLevel(mannerLevel)
                 .member(member)
                 .build();
     }
@@ -99,7 +120,8 @@ public class MatchingRequest extends BaseDateTimeEntity {
     @Builder
     private MatchingRequest(GameMode gameMode, Position mainPosition, Position subPosition, Position wantPosition,
                             Mike mike, Tier soloTier, int soloRank, double soloWinRate, Tier freeTier,
-                            int freeRank, double freeWinRate, Member member) {
+                            int freeRank, double freeWinRate, MatchingType matchingType, int mannerLevel,
+                            Member member) {
         this.gameMode = gameMode;
         this.mainPosition = mainPosition;
         this.subPosition = subPosition;
@@ -111,6 +133,8 @@ public class MatchingRequest extends BaseDateTimeEntity {
         this.freeTier = freeTier;
         this.freeRank = freeRank;
         this.freeWinRate = freeWinRate;
+        this.matchingType = matchingType;
+        this.mannerLevel = mannerLevel;
         this.member = member;
     }
 
