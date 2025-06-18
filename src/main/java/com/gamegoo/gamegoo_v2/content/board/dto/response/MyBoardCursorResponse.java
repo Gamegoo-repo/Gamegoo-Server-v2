9c3bb2c914bf4b6db3196bc022b3bc627997b550
jinhyeong jang <jinhinjala26@naver.com>
2025-06-18 17:43:10 +0900

    ✨ [Feat] 내 게시물 확인 cursor Response 추가

---
+++ src/main/java/com/gamegoo/gamegoo_v2/content/board/dto/response/MyBoardCursorResponse.java
@@ -0,0 +1,42 @@
+package com.gamegoo.gamegoo_v2.content.board.dto.response;
+
+import com.gamegoo.gamegoo_v2.content.board.domain.Board;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class MyBoardCursorResponse {
+
+    List<MyBoardListResponse> myBoards;
+    int size;
+    boolean hasNext;
+    Long nextCursor;
+
+
+    public static MyBoardCursorResponse of(Slice<Board> boardSlice) {
+
+        // Board -> MyBoardListResponse 변환
+        List<MyBoardListResponse> boardList = boardSlice.getContent().stream()
+                .map(MyBoardListResponse::of)
+                .collect(Collectors.toList());
+
+        Long nextCursor = boardSlice.hasNext() && !boardList.isEmpty()
+                ? boardList.get(boardList.size() - 1).getBoardId()
+                :null;
+
+        // DTO 생성
+        return MyBoardCursorResponse.builder()
+                .myBoards(boardList)
+                .size(boardList.size())
+                .hasNext(boardSlice.hasNext())
+                .nextCursor(nextCursor)
+                .build();
+    }
+
+}
