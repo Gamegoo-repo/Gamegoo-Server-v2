e30675d6514e1ee1f79f670a69237f39292a5e50
jinhyeong jang <jinhinjala26@naver.com>
2025-06-25 16:10:39 +0900

    🐛[Fix] nextCursor 필드값 추가 및 테스트 코드 추가

--- src/main/java/com/gamegoo/gamegoo_v2/content/board/dto/response/BoardCursorResponse.java
+++ src/main/java/com/gamegoo/gamegoo_v2/content/board/dto/response/BoardCursorResponse.java
@@ -22,14 +22,17 @@ public class BoardCursorResponse {
                 .map(BoardListResponse::of)
                 .collect(Collectors.toList());
         Long cursorId = null;
+        LocalDateTime nextCursor = null;
         if (!boardSlice.isEmpty()) {
             Board lastBoard = boardSlice.getContent().get(boardSlice.getContent().size() - 1);
             cursorId = lastBoard.getId();
+            nextCursor = lastBoard.getActivityTime();
         }
         return BoardCursorResponse.builder()
                 .boards(boards)
                 .hasNext(boardSlice.hasNext())
                 .cursorId(cursorId)
+                .nextCursor(nextCursor)
                 .build();
     }
 } 
