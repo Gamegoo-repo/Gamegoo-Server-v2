157db0cc5c0e61090010c76845e897b46cf035d7
Eunjin3395 <jennyeunjin@khu.ac.kr>
2025-07-23 14:01:21 +0900

    ♻️ [Refactor] prometheus 로깅 경로 제외 리팩토링

--- src/main/java/com/gamegoo/gamegoo_v2/core/log/LoggingFilter.java
+++ src/main/java/com/gamegoo/gamegoo_v2/core/log/LoggingFilter.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -16,13 +17,17 @@ public class LoggingFilter extends OncePerRequestFilter {
 
     private final LogUtil logUtil;
 
+    private static final Set<String> EXCLUDED_LOGGING_PATHS = Set.of(
+            "/healthcheck",
+            "/actuator/prometheus"
+    );
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         String uri = request.getRequestURI();
 
-        // /healthcheck 경로는 로그 출력 제외
-        if ("/healthcheck".equals(uri) || "/actuator/prometheus".equals(uri)) {
+        if (EXCLUDED_LOGGING_PATHS.contains(uri)) {
             filterChain.doFilter(request, response);
             return;
         }
