cd4706a81b675a2d4ce086ebd1dbe170250cb667
jinhyeong jang <jinhinjala26@naver.com>
2025-08-04 00:09:39 +0900

    :bug: [Fix] try-catch 블록 삭제

--- src/main/java/com/gamegoo/gamegoo_v2/account/member/dto/response/MemberRecentStatsResponse.java
+++ src/main/java/com/gamegoo/gamegoo_v2/account/member/dto/response/MemberRecentStatsResponse.java
@@ -44,45 +44,40 @@ public class MemberRecentStatsResponse {
         if (memberRecentStats == null) {
             return null;
         }
-
-        try {
-            return switch (gameMode) {
-                case SOLO, FAST -> new MemberRecentStatsResponse(
-                        memberRecentStats.getSoloRecTotalWins(),
-                        memberRecentStats.getSoloRecTotalLosses(),
-                        memberRecentStats.getSoloRecWinRate(),
-                        memberRecentStats.getSoloRecAvgKDA(),
-                        memberRecentStats.getSoloRecAvgKills(),
-                        memberRecentStats.getSoloRecAvgDeaths(),
-                        memberRecentStats.getSoloRecAvgAssists(),
-                        memberRecentStats.getSoloRecAvgCsPerMinute(),
-                        memberRecentStats.getSoloRecTotalCs()
-                );
-                case FREE -> new MemberRecentStatsResponse(
-                        memberRecentStats.getFreeRecTotalWins(),
-                        memberRecentStats.getFreeRecTotalLosses(),
-                        memberRecentStats.getFreeRecWinRate(),
-                        memberRecentStats.getFreeRecAvgKDA(),
-                        memberRecentStats.getFreeRecAvgKills(),
-                        memberRecentStats.getFreeRecAvgDeaths(),
-                        memberRecentStats.getFreeRecAvgAssists(),
-                        memberRecentStats.getFreeRecAvgCsPerMinute(),
-                        memberRecentStats.getFreeRecTotalCs()
-                );
-                case ARAM -> new MemberRecentStatsResponse(
-                        memberRecentStats.getAramRecTotalWins(),
-                        memberRecentStats.getAramRecTotalLosses(),
-                        memberRecentStats.getAramRecWinRate(),
-                        memberRecentStats.getAramRecAvgKDA(),
-                        memberRecentStats.getAramRecAvgKills(),
-                        memberRecentStats.getAramRecAvgDeaths(),
-                        memberRecentStats.getAramRecAvgAssists(),
-                        memberRecentStats.getAramRecAvgCsPerMinute(),
-                        memberRecentStats.getAramRecTotalCs()
-                );
-            };
-        } catch (jakarta.persistence.EntityNotFoundException e) {
-            return null;
-        }
+        return switch (gameMode) {
+            case SOLO, FAST -> new MemberRecentStatsResponse(
+                    memberRecentStats.getSoloRecTotalWins(),
+                    memberRecentStats.getSoloRecTotalLosses(),
+                    memberRecentStats.getSoloRecWinRate(),
+                    memberRecentStats.getSoloRecAvgKDA(),
+                    memberRecentStats.getSoloRecAvgKills(),
+                    memberRecentStats.getSoloRecAvgDeaths(),
+                    memberRecentStats.getSoloRecAvgAssists(),
+                    memberRecentStats.getSoloRecAvgCsPerMinute(),
+                    memberRecentStats.getSoloRecTotalCs()
+            );
+            case FREE -> new MemberRecentStatsResponse(
+                    memberRecentStats.getFreeRecTotalWins(),
+                    memberRecentStats.getFreeRecTotalLosses(),
+                    memberRecentStats.getFreeRecWinRate(),
+                    memberRecentStats.getFreeRecAvgKDA(),
+                    memberRecentStats.getFreeRecAvgKills(),
+                    memberRecentStats.getFreeRecAvgDeaths(),
+                    memberRecentStats.getFreeRecAvgAssists(),
+                    memberRecentStats.getFreeRecAvgCsPerMinute(),
+                    memberRecentStats.getFreeRecTotalCs()
+            );
+            case ARAM -> new MemberRecentStatsResponse(
+                    memberRecentStats.getAramRecTotalWins(),
+                    memberRecentStats.getAramRecTotalLosses(),
+                    memberRecentStats.getAramRecWinRate(),
+                    memberRecentStats.getAramRecAvgKDA(),
+                    memberRecentStats.getAramRecAvgKills(),
+                    memberRecentStats.getAramRecAvgDeaths(),
+                    memberRecentStats.getAramRecAvgAssists(),
+                    memberRecentStats.getAramRecAvgCsPerMinute(),
+                    memberRecentStats.getAramRecTotalCs()
+            );
+        };
     }
 }
