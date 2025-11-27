534738463fa2472e10e0c63c7f8a403b27c31296
jinhyeong jang <jinhinjala26@naver.com>
2025-11-27 15:06:34 +0900

    [refactor] 불필요한 import문 제거

--- src/main/java/com/gamegoo/gamegoo_v2/external/riot/service/RiotRecordService.java
+++ src/main/java/com/gamegoo/gamegoo_v2/external/riot/service/RiotRecordService.java
@@ -17,8 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
-import lombok.Getter;
-import lombok.Builder;
+
 
 import java.time.Instant;
 import java.time.LocalDateTime;
