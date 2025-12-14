856cdce0862047dceab8e3453d31eae31975dc8c
Eunjin3395 <jennyeunjin@khu.ac.kr>
2025-12-14 18:19:10 +0900

    ♻️ [Refactor] 매너평가 시스템 메시지 스케줄러 주기 조정

--- src/main/java/com/gamegoo/gamegoo_v2/core/scheduler/SystemMessageScheduler.java
+++ src/main/java/com/gamegoo/gamegoo_v2/core/scheduler/SystemMessageScheduler.java
@@ -22,7 +22,7 @@ public class SystemMessageScheduler {
     private final MatchingRecordRepository matchingRecordRepository;
     private final MannerMessageHandler mannerMessageHandler;
 
-    private static final Long MANNER_MESSAGE_TIME = 30 * 60L; // 30 * 60초
+    private static final Long MANNER_MESSAGE_TIME = 2 * 60L; // 2 * 60초
 
     @Scheduled(fixedRate = 60 * 1000) // 60초 주기로 실행
     public void mannerSystemMessageRun() {
