9e97e5dd97760c2f2e5ce027e8e4dc556a260d3c
Eunjin3395 <jennyeunjin@khu.ac.kr>
2025-12-20 17:14:33 +0900

    ✅ [Test] 친구 요청 관련 테스트코드 수정

--- src/main/java/com/gamegoo/gamegoo_v2/core/event/listener/FriendRequestEventListener.java
+++ src/main/java/com/gamegoo/gamegoo_v2/core/event/listener/FriendRequestEventListener.java
@@ -6,7 +6,6 @@ import com.gamegoo.gamegoo_v2.core.event.AcceptFriendRequestEvent;
 import com.gamegoo.gamegoo_v2.core.event.RejectFriendRequestEvent;
 import com.gamegoo.gamegoo_v2.core.event.SendFriendRequestEvent;
 import com.gamegoo.gamegoo_v2.notification.service.NotificationService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -28,7 +27,6 @@ public class FriendRequestEventListener {
      * @param event event
      */
     @Async
-    @Transactional
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleSendFriendRequestEvent(SendFriendRequestEvent event) {
         try {
